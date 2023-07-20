d40a69898f80676212320df05e77ff1aeee8d914
Luis Fernando <guffenix@gmail.com>
2023-07-20 00:03:50 -0500

    chore: update alumno service

--- src/services/alumnoServices.js
+++ src/services/alumnoServices.js
@@ -1,73 +1,86 @@
-var mongoose = require("mongoose");
-var Alumno = mongoose.model('Alumno', Alumno);
+const Alumno = require('../models/alumno')
 
-async function crearAlumno(alumno){
-    return await alumno.save(function(err){
-        if (err) return err;
-    });
+async function crearAlumno(alumno) {
+  return await alumno.save(function (err) {
+    if (err) return err
+  })
 }
 
-async function obtenerAlumnos(){
-    return await Alumno.find();
+async function obtenerAlumnos() {
+  return await Alumno.find()
 }
 
 async function buscarAlumnoByCI(_cedula) {
-    return await Alumno.find({ cedula: _cedula });
+  return await Alumno.find({ cedula: _cedula })
 }
 
 async function buscarTelefonosByCI(_cedula) {
-    return await Alumno.find({ cedula: _cedula })
-    .select("telefono")
-    .exec();
+  return await Alumno.find({ cedula: _cedula }).select('telefono').exec()
 }
 
-async function eliminarAlumnoByCI(_cedula){
-    return await Alumno.deleteOne({ cedula: _cedula }, function(err){
-        if (err) return err;
-    });
+async function eliminarAlumnoByCI(_cedula) {
+  return await Alumno.deleteOne({ cedula: _cedula }, function (err) {
+    if (err) return err
+  })
 }
 
-async function eliminarAlumnoByID(_id){
-    return await Alumno.deleteOne({ _id: _id }, function(err){
-        if (err) return err;
-    });
-    
+async function eliminarAlumnoByID(_id) {
+  return await Alumno.deleteOne({ _id: _id }, function (err) {
+    if (err) return err
+  })
 }
 
-async function actualizarAlumnoByCI(_cedula, _edad){
-    return await Alumno.updateOne({ cedula: _cedula }, { edad: _edad},function(err){
-        if (err) return err;
-    });
-    
+async function actualizarAlumnoByCI(_cedula, _edad) {
+  return await Alumno.updateOne(
+    { cedula: _cedula },
+    { edad: _edad },
+    function (err) {
+      if (err) return err
+    }
+  )
 }
 
-async function asignaDireccionByCI(_cedula, _direccion){
-    return await Alumno.updateOne({ cedula: _cedula }, { direccion: _direccion},function(err){
-        if (err) return err;
-    });   
+async function asignaDireccionByCI(_cedula, _direccion) {
+  return await Alumno.updateOne(
+    { cedula: _cedula },
+    { direccion: _direccion },
+    function (err) {
+      if (err) return err
+    }
+  )
 }
 
-async function agregarTelefonoByCI(_cedula, _telefono){
-    return await Alumno.findOneAndUpdate({ cedula: _cedula }, { $push: {telefono: _telefono}},{safe: true, upsert: true},function(err, telefono){
-        if (err) return err;
-    });   
+async function agregarTelefonoByCI(_cedula, _telefono) {
+  return await Alumno.findOneAndUpdate(
+    { cedula: _cedula },
+    { $push: { telefono: _telefono } },
+    { safe: true, upsert: true },
+    function (err, telefono) {
+      if (err) return err
+    }
+  )
 }
 
-async function eliminarTelefonoByCI(_cedula, _telefono){
-    return await Alumno.findOneAndUpdate({ cedula: _cedula }, { $pull: {telefono: {numero: _telefono}}},{safe: true, upsert: true},function(err){
-        if (err) return err;
-    });   
+async function eliminarTelefonoByCI(_cedula, _telefono) {
+  return await Alumno.findOneAndUpdate(
+    { cedula: _cedula },
+    { $pull: { telefono: { numero: _telefono } } },
+    { safe: true, upsert: true },
+    function (err) {
+      if (err) return err
+    }
+  )
 }
 
-module.exports  = {
-    crear: crearAlumno,
-    buscar: obtenerAlumnos,
-    buscarByCI: buscarAlumnoByCI,
-    buscarTelefonosByCI: buscarTelefonosByCI,
-    actualizarByCI: actualizarAlumnoByCI,
-    eliminarByCI: eliminarAlumnoByCI,
-    eliminarAlumnoByID: eliminarAlumnoByID,
-    asignaDireccionByCI: asignaDireccionByCI,
-    agregarTelefonoByCI: agregarTelefonoByCI,
-    eliminarTelefonoByCI:eliminarTelefonoByCI
-};  
+module.exports = {
+  crear: crearAlumno,
+  buscar: obtenerAlumnos,
+  buscarByCI: buscarAlumnoByCI,
+  buscarTelefonosByCI: buscarTelefonosByCI,
+  actualizarByCI: actualizarAlumnoByCI,
+  eliminarByCI: eliminarAlumnoByCI,
+  eliminarAlumnoByID: eliminarAlumnoByID,
+  asignaDireccionByCI: asignaDireccionByCI,
+  agregarTelefonoByCI: agregarTelefonoByCI,
+  eliminarTelefonoByCI: eliminarTelefonoByCI,
+}
