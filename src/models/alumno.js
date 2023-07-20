d0e08f8ad1cf83697f3e3e71591bb3783fac616e
Luis Fernando <guffenix@gmail.com>
2023-07-20 00:02:50 -0500

    chore: update alumno model

--- src/models/alumno.js
+++ src/models/alumno.js
@@ -1,17 +1,17 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-var Schema = mongoose.Schema;
+const AlumnoSchema = new Schema({
+  cedula: String,
+  nombre: String,
+  apellido: String,
+  edad: Number,
+  estatura: Number,
+  fechaNacimiento: Date,
+  direccion: { calle: String, numero: String, interseccion: String },
+  telefono: [{ _id: false, numero: String, descripcion: String }],
+})
 
-var Alumno = new Schema({
-    cedula: String,
-    nombre: String,
-    apellido: String,
-    edad: Number,
-    estatura: Number,
-    fechaNacimiento: Date,
-    direccion: { calle: String, numero: String, interseccion:String },
-    telefono:[{_id: false, numero:String, descripcion:String}]
-});
+const AlumnoModel = mongoose.model('Alumno', AlumnoSchema)
 
-module.exports = mongoose.model('Alumno', Alumno);
-
+module.exports = AlumnoModel
